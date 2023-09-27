7831a64483df5248086230314d2f7ce52b2c2a98
YehorBondarenko96 <egorbondarenko1996@gmail.com>
2023-09-27 15:49:37 +0300

    2.2///27.09.23

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,10 @@ import throttle from "lodash.throttle";
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframe);
 
-player.on('play', () => {
-    const time = JSON.parse(localStorage.getItem("videoplayer-current-time"));
-    if (time !== null) {
-        player.setCurrentTime(time);
-    }
-});
+const time = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+if (time !== null) {
+    player.setCurrentTime(time);
+};
 
     player.on('timeupdate', throttle(function(data) {
         localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
